30cd6030954eb1ff3aa17097f95008a7bca15a3e
Marvellous Ogege <marvellousogege@gmail.com>
2022-06-16 16:37:52 -0700

    Header file update

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -23,6 +23,6 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
+int match_finder(char *h, char *n);
 #endif
 
